ac8f544d58cc763a9d72e507d172dfb5666b11b0
albert-dimitrov <dimitrov.albert420@gmail.com>
2025-05-31 21:48:53 +0300

    Add: Product categorie page with their images css html and view for this page

--- JewelryShop/jewelries/urls.py
+++ JewelryShop/jewelries/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from JewelryShop.jewelries import views
 
 urlpatterns = [
+    path('categories/', views.category_choose_page, name='categories'),
+    path('collections/<str:category>/', views.JewelriesProductShowView.as_view(), name='collections'),
     path('add/', views.JewelriesAddView.as_view(), name='jewelry_add'),
     path('<int:pk>/', include([
         path('details', views.JewelriesDetailsView.as_view(), name='jewelry_details'),
--- JewelryShop/jewelries/views.py
+++ JewelryShop/jewelries/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView, DetailView, DeleteView
+from django.views.generic import CreateView, UpdateView, DetailView, DeleteView, ListView
 
 from JewelryShop.jewelries.forms import JewelryCreateForm, JewelryEditForm
 from JewelryShop.jewelries.models import Jewelry
@@ -46,3 +46,32 @@ class JewelriesDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.groups.filter(name='Moderation Group').exists()
+
+
+class JewelriesProductShowView(ListView):
+    model = Jewelry
+    template_name = ''
+
+    def get_queryset(self):
+        product_category = self.kwargs.get('category')
+        return Jewelry.objects.filter(category=product_category)
+
+
+def category_choose_page(request):
+    categories = [
+        {'name': 'Rings', 'image': 'ring.jpg'},
+        {'name': 'Necklaces', 'image': 'necklace.jpg'},
+        {'name': 'Earrings', 'image': 'earring.jpg'},
+        {'name': 'Bracelets', 'image': 'bracelet.jpg'},
+        {'name': 'Anklets', 'image': 'anklet.jpg'},
+        {'name': 'Brooches & Pins', 'image': 'brooch.jpg'},
+        {'name': 'Charms', 'image': 'charms.jpg'},
+        {'name': 'Jewelry Sets', 'image': 'set.jpg'},
+        {'name': 'Watches', 'image': 'watch.jpg'},
+    ]
+
+    context = {
+        'categories': categories,
+    }
+
+    return render(request, 'jewelries/choose-category.html', context)
